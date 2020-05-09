de26db09cf535725e2e99de62c6d17f19681a9b3
Javier Munoz <javimudi@users.noreply.github.com>
2020-05-09 19:29:09 +0200

    wip: first files

---
+++ hello/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -f ${GENDEV}/sgdk/mkfiles/makefile.gen clean all
---
+++ hello/src/hello.c
@@ -0,0 +1,8 @@
+#include <genesis.h>
+
+int main(u16 hard)
+{
+    VDP_drawText("Hello World!", 10, 13);
+
+    return 0;
+}
